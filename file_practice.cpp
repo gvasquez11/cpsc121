cc3329af35495208f463cbfb2d1a9bdf170bd2ba
Gregory Vasquez <vasquez.greg16@gmail.com>
2019-02-05 14:50:44 -0800

    updated

--- file_practice.cpp
+++ file_practice.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+int main()
+{
+  ofstream myFile;
+  myFile.open("door.txt");
+  myFile << "I'm writing text to a file! Woooooo!" << endl;
+  myFile.close();
+
+  return 0;
+}
