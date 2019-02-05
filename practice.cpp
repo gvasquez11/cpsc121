3ead6752bb1179a9ca870951ae4c9d7640a1c118
Gregory Vasquez <vasquez.greg16@gmail.com>
2019-02-05 12:43:40 -0800

    updated .cpp file

--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+int main()
+{
+
+  int num;
+  std::cout << "Enter a number: ";
+  std::cin >> num;
+
+  if(num % 2 == 0)
+  std::cout << "Your number is even!" << std::endl;
+  else
+  std::cout << "Your number is odd!" << std::endl;
+
+
+
+  return 0;
+}
