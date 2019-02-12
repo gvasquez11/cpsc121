31f344e69ff93eb7714c1bd0cd155643c7d56c22
Gregory Vasquez <vasquez.greg16@gmail.com>
2019-02-12 14:08:14 -0800

    update

--- readingFromFile.cpp
+++ readingFromFile.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
 
+using namespace std;
+
+int main()
+{
+  string name;
+  ifstream file;
+  file.open("names.txt");
+
+  while(!file.eof())
+  {
+      getline(file, name);
+      cout << name << endl;
+  }
+  return 0;
+}
